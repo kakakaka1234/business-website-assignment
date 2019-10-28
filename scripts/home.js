8dab87cd64e17e42b5c1241337766b0993db0cbd
Luong Hoang <43042403+kakakaka1234@users.noreply.github.com>
2019-10-28 13:51:30 -0600

    update

--- scripts/home.js
+++ scripts/home.js
@@ -1,13 +1,1 @@
 
-	function addNumbers()
-	{
-		var val1 = parseInt(document.getElementById("value1").value);
-		var val2 = parseInt(document.getElementById("value2").value);
-    var val3 = parseInt(document.getElementById("value3").value);
-    var val4 = parseInt(document.getElementById("value4").value);
-    var val5 = parseInt(document.getElementById("value5").value);
-    var val6 = parseInt(document.getElementById("value6").value);
-    var val7 = parseInt(document.getElementById("value7").value);
-		var ansD = document.getElementById("answer");
-		ansD.value = val1 + val2 + val3 +val4 +val5 +val6+ val7;
-	}
